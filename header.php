936c4b7e3b603ac6babf380df3aac87ef545fb52
PoloDeNaranja <115736861+PoloDeNaranja@users.noreply.github.com>
2023-03-05 11:21:04 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang = "es">
+<head>    
+    <meta charset="UTF-8">
+    <tittle>Header</tittle>
+    <!--Esto es para moviles -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="estiloHeader.css">
+</head>
+<body>
+    <header>
+        <a href="#" classs="logo">
+        <img src="logo.png" alt = "logo">
+        </a>
+        <nav>
+            <a href="" class="nav-link">Inicio</a>
+            <a href="" class="nav-link">Ofertas</a>
+            <!-- <ul>
+                    <li><a href="">1º Cuatrimestre</li>
+                    <li><a href="">2º Cuatrimestre</li>
+                    <li><a href="">Extracurriculares</li>     
+            </ul> -->
+            <a href="" class="nav-link">Noticias</a>
+            <a href="">
+            <img class="notificacion"src="notificacion.png" alt = "notificacion">
+            </a>   
+            <a href="">
+            <img class="perfil"src="perfil.png" alt = "logo">
+            </a>
+        </nav>
+        
+    </header>
+
+</body> 
+</html>
