817291574435265a61065e697f4a001b58bb4d1b
PoloDeNaranja <115736861+PoloDeNaranja@users.noreply.github.com>
2023-03-07 22:55:34 +0100

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang = "es">
+<head>    
+    <meta charset="UTF-8">
+    <tittle>Register</tittle>
+    <!--Esto es para moviles -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="estiloRegister.css">
+    <script src="https://kit.fontawesome.com/0d34bde1b9.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <form action="">
+        <h1 class="titulo">Create your account!</h1>
+        <label>
+            <i class="fa-solid fa-user"></i>
+            <input placeholder="Enter your username" type="text">
+        </label>
+        <label>
+           <i class="fa-solid fa-envelope"></i>
+            <input placeholder="Enter your e-mail" type="text">
+        </label>
+        <label>
+            <i class="fa-solid fa-lock"></i>
+            <input placeholder="Enter your Password" type="password">
+        </label>
+        <label>
+            <i class="fa-solid fa-lock"></i>
+            <input placeholder="Confirm password" type="password">
+        </label>
+
+        <a href="" class="tiene">Already have an account?</a>
+        
+        <button class="button reg">Register</button>
+        <button class="button log">Login</button>
+        
+    </form>        
+
+</body> 
+</html>
