336fb8bb62b273b25a82670010a463d0abac2705
PoloDeNaranja <115736861+PoloDeNaranja@users.noreply.github.com>
2023-03-08 19:26:56 +0100

    Add files via upload

--- registro.php
+++ registro.php
@@ -1,3 +1,7 @@
+
+
+
+
 <!DOCTYPE html>
 <html lang = "es">
 <head>    
@@ -6,26 +10,30 @@
     <!--Esto es para moviles -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" href="estiloRegister.css">
-    <script src="https://kit.fontawesome.com/0d34bde1b9.js" crossorigin="anonymous"></script>
+   <script src="register.js" crossorigin="anonymous"></script>
+      
 </head>
 <body>
     <form action="">
         <h1 class="titulo">Create your account!</h1>
         <label>
            <i class="fa-solid fa-user"></i>
-            <input placeholder="Enter your username" type="text">
+            <input placeholder="Enter your username" type="text" name="name" required>
         </label>
         <label>
            <i class="fa-solid fa-envelope"></i>
-            <input placeholder="Enter your e-mail" type="text">
+            <input placeholder="Enter your e-mail" type="text" name="email" required>
         </label>
         <label>
             <i class="fa-solid fa-lock"></i>
-            <input placeholder="Enter your Password" type="password">
+            <input type="password" placeholder="Enter your password"  name= "password" id="password" required>
+           <button type="button" style="margin-left: 10px" id="show-password">a</button>
+            <img src=""alt="" class="icon">
         </label>
         <label>
             <i class="fa-solid fa-lock"></i>
-            <input placeholder="Confirm password" type="password">
+            <input placeholder="Confirm password" type="password" name="password" id="confirm" required>
+           <button type="button" style="margin-left: 13px" id="show">a</button>
         </label>
 
         <a href="" class="tiene">Already have an account?</a>
@@ -34,6 +42,6 @@
         <button class="button log">Login</button>
         
     </form>        
-
+<script src="https://kit.fontawesome.com/0d34bde1b9.js" crossorigin="anonymous"></script>
 </body> 
 </html>
