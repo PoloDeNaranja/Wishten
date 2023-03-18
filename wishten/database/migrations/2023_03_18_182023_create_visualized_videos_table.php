11bb06501ee2c2eeac08ab451567869e50030788
roberto-tejedor <118179523+roberto-tejedor@users.noreply.github.com>
2023-03-18 19:31:01 +0100

    Migraciones de tablas Videos, subjects y visualized_videos

---
+++ wishten/database/migrations/2023_03_18_182023_create_visualized_videos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visualized_videos', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('video_id')->constrained('videos')->onUpdate('cascade')->onDelete('cascade');
+            $table->boolean('fav');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visualized_videos');
+    }
+};
