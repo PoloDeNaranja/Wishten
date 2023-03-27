a4b481407875e93b9652ab47e5b7dbccef6de9df
PoloDeNaranja <115736861+PoloDeNaranja@users.noreply.github.com>
2023-03-27 19:29:08 +0200

    Add files via upload

---
+++ Popup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel = "stylesheet" href="estiloPopup.css">
+<script src="Popup.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <button id="openPopup">Change Password</button>
+    <div id="PopupWindow" class="popup">
+        <div class="popupContent">
+            <span class="closePopup">&times;</span>
+            <p>Lo que sea que quieras meter en el Popup</p>
+        </div>
+    </div>
+
+</body>
+</html>
