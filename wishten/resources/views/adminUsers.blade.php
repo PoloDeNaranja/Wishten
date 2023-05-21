fa7a4fba07f2bc5519901c662dc00e85161e10f4
roberto-tejedor <118179523+roberto-tejedor@users.noreply.github.com>
2023-05-21 20:57:58 +0200

    Actualización en la vista de adminUsers Ahora la información de los usuarios se ve en modo tabla.

--- wishten/resources/views/adminUsers.blade.php
+++ wishten/resources/views/adminUsers.blade.php
@@ -19,8 +19,17 @@
 
     <h1>Admin Users</h1>
     <div id="userList">
-        @foreach ($users as $user)
-            {{-- <div class="user-data">
+        <table id="userInfo">
+            <tr class="header">
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Created at</th>
+                <th>Updated at</th>
+                <th>Action</th>
+            </tr>
+            @foreach ($users as $user)
+                {{-- <div class="user-data">
                 <form action="{{ route('profile.update_info', Auth::id()) }}" method="post">
                     @csrf
                     <input type="text" name="name" value="{{ $user->name }}">
@@ -34,68 +43,79 @@
                 </form>
                 <button>Ban</button>
             </div> --}}
-            <div class="userList-item">
-                <label>{{ $user->name }}</label>
-                <button class="openPopup button">Modify</button>
-                <form id="delete-user" action="{{ route('user.delete', $user->id) }}" method="post">
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->created_at }}</td>
+                    <td>{{ $user->updated_at }}</td>
+
+                    <td>
+                        <form id="action-buttons" action="{{ route('user.delete', $user->id) }}" method="post">
+                            @csrf
+                            <button class="openPopup button" type="button">Modify</button>
+                            <button class="delete-user button red" type="submit">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+    @foreach ($users as $user)
+        <div id="PopupWindow" class="popup">
+            <div class="popupContent">
+                <span class="closePopup">&times;</span>
+                <h3>Modify user "{{ $user->name }}" data</h3>
+                <form action="{{ route('profile.update_pic', $user->id) }}" method="POST" enctype="multipart/form-data">
                     @csrf
-                    <button class="delete-user button red">Delete</button>
+                    <label for="new_pic" class="profile_pic">
+                        <input class="form-control" type="file" name="new_pic" id="new_pic" accept=".jpg,.jpeg,.png">
+                        @if ($user->profile_pic != 'None')
+                            <img src="{{ url('storage/' . $user->profile_pic) }}" alt="mdo" width="60"
+                                height="60">
+                        @else
+                            <i class="fa fa-user-circle-o fa-4x" aria-hidden="true"></i>
+                        @endif
+                    </label>
+                    <button type="submit" class="button">Upload</button>
+                </form>
+
+                <form action="{{ route('profile.delete_pic', $user->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <button type="submit" class="button red">Delete Picture</button>
+                </form>
+
+                <form action="{{ route('profile.update_info', $user->id) }}" method="POST">
+                    @csrf
+                    <label>
+                        <i class="fa-solid fa-user"></i>
+                        <input type="text" name="name" value="{{ $user->name }}">
+                        @if ($errors->has('name'))
+                            <span class="error-text">{{ $errors->first('name') }}</span>
+                        @endif
+                    </label>
+                    <label>
+                        <i class="fa-solid fa-envelope"></i>
+                        <input type="email" name="email" aria-describedby="emailHelp" value="{{ $user->email }}">
+                        @if ($errors->has('email'))
+                            <span class="error-text">{{ $errors->first('email') }}</span>
+                        @endif
+                    </label>
+                    <button type="submit" class="button">Save changes</button>
+                </form>
+
+                <form action="{{ route('adminUsers.ban', $user->id) }}" method="post">
+                    @csrf
+                    @if ($user->ban)
+                        <button type="submit" class="button">Unban</button>
+                    @else
+                        <button type="submit" class="button red">Ban</button>
+                    @endif
                 </form>
             </div>
-            <div id="PopupWindow" class="popup">
-                <div class="popupContent">
-                    <span class="closePopup">&times;</span>
-                    <h3>Modify user "{{ $user->name }}" data</h3>
-                    <form action="{{ route('profile.update_pic', $user->id) }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <label for="new_pic" class="profile_pic">
-                            <input class="form-control" type="file" name="new_pic" id="new_pic" accept=".jpg,.jpeg,.png">
-                            @if ($user->profile_pic != 'None')
-                                <img src="{{ url('storage/'.$user->profile_pic) }}" alt="mdo" width="60" height="60">
-                            @else
-                                <i class="fa fa-user-circle-o fa-4x" aria-hidden="true"></i>
-                            @endif
-                        </label>
-                        <button type="submit" class="button">Upload</button>
-                    </form>
-
-                    <form action="{{ route('profile.delete_pic', $user->id) }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <button type="submit" class="button red">Delete Picture</button>
-                    </form>
-
-                    <form action="{{ route('profile.update_info', $user->id) }}" method="POST">
-                        @csrf
-                        <label>
-                            <i class="fa-solid fa-user"></i>
-                            <input type="text" name="name" value="{{ $user->name }}">
-                            @if ($errors->has('name'))
-                                <span class="error-text">{{ $errors->first('name') }}</span>
-                            @endif
-                        </label>
-                        <label>
-                            <i class="fa-solid fa-envelope"></i>
-                            <input type="email" name="email" aria-describedby="emailHelp" value="{{ $user->email }}">
-                            @if ($errors->has('email'))
-                                <span class="error-text">{{ $errors->first('email') }}</span>
-                            @endif
-                        </label>
-                        <button type="submit" class="button">Save changes</button>
-                    </form>
-
-                    <form action="{{ route('adminUsers.ban', $user->id) }}" method="post">
-                        @csrf
-                        @if ($user->ban)
-                            <button type="submit" class="button">Unban</button>
-                        @else
-                            <button type="submit" class="button red">Ban</button>
-                        @endif
-                    </form>
-                </div>
-            </div>
-        @endforeach
-    </div>
-    <button class="openPopup button">Add user</button>
+        </div>
+    @endforeach
+    <button id="addUser" class="openPopup button">Add user</button>
     <div id="PopupWindow" class="popup">
         <div class="popupContent">
             <span class="closePopup">&times;</span>
@@ -103,14 +123,14 @@
                 @csrf
                 <h1 class="titulo">Create your account!</h1>
                 <label>
-                   <i class="fa-solid fa-user" for="name"></i>
+                    <i class="fa-solid fa-user" for="name"></i>
                     <input placeholder="Enter your username" type="text" name="name" id="name" required>
                     @if ($errors->has('name'))
                         <span class="error-text">{{ $errors->first('name') }}</span>
                     @endif
                 </label>
                 <label>
-                   <i class="fa-solid fa-envelope" for="email"></i>
+                    <i class="fa-solid fa-envelope" for="email"></i>
                     <input placeholder="Enter your e-mail" type="email" name="email" id="email" required>
                     @if ($errors->has('email'))
                         <span class="error-text">{{ $errors->first('email') }}</span>
@@ -118,7 +138,7 @@
                 </label>
                 <label>
                     <i class="fa-solid fa-lock" for="password"></i>
-                    <input type="password" placeholder="Enter your password"  name= "password" id="password" required>
+                    <input type="password" placeholder="Enter your password" name="password" id="password" required>
                     <button type="button" style="margin-left: 10px" id="show-password">
                         <i class="fa fa-eye" aria-hidden="true"></i>
                     </button>
@@ -128,7 +148,8 @@
                 </label>
                 <label>
                     <i class="fa-solid fa-lock" for="confirm"></i>
-                    <input placeholder="Confirm password" type="password" name="password_confirmation" id="confirm" required>
+                    <input placeholder="Confirm password" type="password" name="password_confirmation" id="confirm"
+                        required>
                     <button type="button" style="margin-left: 10px" id="show">
                         <i class="fa fa-eye" aria-hidden="true"></i>
                     </button>
@@ -143,6 +164,3 @@
     </div>
 
 @endsection
-
-
-
