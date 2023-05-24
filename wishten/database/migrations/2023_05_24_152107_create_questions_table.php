e5c57d7f500a583ed2d9c352bb27695f7f7fe09f
roberto-tejedor <118179523+roberto-tejedor@users.noreply.github.com>
2023-05-24 18:10:24 +0200

    Migraciones básicas para los cuestionarios

---
+++ wishten/database/migrations/2023_05_24_152107_create_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->foreignId('video_id')->constrained('videos')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('question_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
