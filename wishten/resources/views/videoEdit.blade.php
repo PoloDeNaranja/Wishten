10cc5f9669275771b70cb2a79717fbcce3e0958b
roberto-tejedor <118179523+roberto-tejedor@users.noreply.github.com>
2023-06-04 20:19:14 +0200

    Nuevo estilo para la vista de un video

--- wishten/resources/views/videoEdit.blade.php
+++ wishten/resources/views/videoEdit.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<video width="320" height="240" controls src="{{ url('storage/'.$video->file_path) }}"></video>
+<video width="320" height="240" controls src="{{ url('storage/'.$video->video_path) }}"></video>
 <p>{{ $video->description }}</p>
 <form action="{{ route('video.delete', ['video' => $video->id, 'admin' => 0]) }}" method="post">
     @csrf
--- wishten/resources/views/videoWatch.blade.php
+++ wishten/resources/views/videoWatch.blade.php
@@ -2,14 +2,44 @@
 
 @section('title', $video->title)
 
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/videoWatchStyle.css') }}" />
+@endsection
+
 @section('content')
 
-<video width="320" height="240" controls src="{{ url('storage/'.$video->video_path) }}"></video>
-<p>{{ $video->description }}</p>
-<p>Subject: {{ $video->subject_id }}</p>
+<div class="video-display">
+    <div class="video-view">
+        @if (Auth::id() == $video->owner_id)
+            <a class="button" href="{{ route('video.edit', ['video'=>$video->id]) }}">
+                <i class="fa-regular fa-pen-to-square"></i>
+                Edit
+            </a>
+        @endif
+        <video controls src="{{ url('storage/'.$video->video_path) }}"></video>
+        <div class="video-info">
+            <h3 class="video-title">{{ $video->title }}</h3>
+            <a class="video-subject" href="videos?subject_name={{ $video->subject->name }}">{{ $video->subject->name }}</a>
+            <p class="video-desc">{{ $video->description }}</p>
+        </div>
+    </div>
+    <div class="related-videos">
+        @foreach ($video->subject->videos as $related_video)
+            @if ($related_video->id != $video->id)
+                <div class="video-card">
+                    <img src="{{ url('storage/' . $related_video->thumb_path) }}" alt="{{ $related_video->title }}">
+                    <a href="{{ route('video.watch', ['video'=>$related_video->id]) }}"></a>
+                    <h3>{{ $related_video->title }}</h3>
+                    <p>{{ $related_video->user->name }}</p>
+                </div>
+            @endif
+        @endforeach
+    </div>
 
-@if (Auth::id() == $video->owner_id)
-    <a href="{{ route('video.edit', ['video'=>$video->id]) }}">Edit</a>
-@endif
+</div>
+
+
+
+
 
 @endsection
