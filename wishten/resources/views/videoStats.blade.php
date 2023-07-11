58f1fd6a292eabeab11ea1a4f77d7537b227f103
roberto-tejedor <118179523+roberto-tejedor@users.noreply.github.com>
2023-07-11 20:51:53 +0200

    Añadidos gráficos Gráficos para visualizar el número de usuarios que han seleccionado cada respuesta, usando Char.js

--- wishten/resources/views/videoStats.blade.php
+++ wishten/resources/views/videoStats.blade.php
@@ -2,6 +2,13 @@
 
 @section('title', 'Stats: '.$video->title)
 
+@section('css')
+<link rel="stylesheet" type="text/css" href="{{ url('/css/videoStatsStyle.css') }}">
+@endsection
+
+@section('js')
+<script  src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+@endsection
 
 @section('content')
 @include('layouts.messages')
@@ -14,9 +21,43 @@
     <h2>Quiz stats:</h2>
     @foreach ($video->questions->sortBy('question_time') as $question)
         <h3>{{ $question->text }}</h3>
-        @foreach ($question->answers as $answer)
-            <p>{{ $answer->text }}: {{ $answer->users->count() }}</p>
-        @endforeach
+        <div style="width:100%;max-width:600px">
+            <canvas id="answers-{{ $question->id }}" ></canvas>
+        </div>
+    <script>
+        // script basado en https://www.w3schools.com/js/js_graphics_chartjs.asp
+        const answers{{ $question->id }} = [
+            @foreach ($question->answers as $answer)
+            "{{ preg_replace('/[\']/', '', $answer->text)}} @if($answer->is_correct)[CORRECT] @endif",
+            @endforeach];
+        const users{{ $question->id }} = [@foreach ($question->answers as $answer)"{{ $answer->users->count() }}",@endforeach];
+        const colors{{ $question->id }} = [
+          "#b91d47",
+          "#00aba9",
+          "#2b5797",
+          "#e8c3b9",
+          "#1e7145"
+        ];
+
+        new Chart("answers-{{ $question->id }}", {
+          type: "doughnut",
+          data: {
+            labels: answers{{ $question->id }},
+            datasets: [{
+              backgroundColor: colors{{ $question->id }},
+              data: users{{ $question->id }}
+            }]
+          },
+          options: {
+            title: {
+              display: true,
+              text: "Answers for question: {{ preg_replace('/[\']/', '', $question->text) }}"
+            }
+          }
+        });
+        </script>
+
     @endforeach
+
 @endif
 @endsection
